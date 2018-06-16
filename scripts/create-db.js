1588f58b99d6a72c260f6e34e9ade44144c45f13
Roberto Manzella <zelphir@gmail.com>
2018-06-16 18:39:26 +0100

    Add basic setup for linting, transpiling and testing

--- scripts/create-db.js
+++ scripts/create-db.js
@@ -1,9 +1,10 @@
 const faker = require('faker')
 const db = {
   transactions: [...Array(1000)].map((_, i) => {
-    const transaction = {id:i+1, ...faker.helpers.createTransaction()}
+    const transaction = { id: i + 1, ...faker.helpers.createTransaction() }
     transaction.amount = +transaction.amount
     return transaction
   })
 }
-console.log(JSON.stringify(db, null, 2))
+
+console.log(JSON.stringify(db, null, 2)) // eslint-disable-line
